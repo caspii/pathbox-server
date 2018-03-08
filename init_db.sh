77a231b21477ed9432abc313bddf734d558c2925
Caspar <caspar.wrede@gmail.com>
2018-03-08 19:35:02 +0100

    Made perfect

--- init_db.sh
+++ init_db.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # This should really not be done on production. Add a test to prevent this
 . venv/bin/activate
 export FLASK_APP=app.py
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 . venv/bin/activate
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
