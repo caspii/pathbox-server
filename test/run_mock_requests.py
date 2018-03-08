2fde2684e3ae696121dcc9d07f5cf1b88c9d90ed
Caspar <caspar.wrede@gmail.com>
2018-03-08 22:01:10 +0100

    Dynamic dates

--- test/run_mock_requests.py
+++ test/run_mock_requests.py
@@ -1,14 +1,17 @@
 # Generate a bunch of mock requests to the server
 import requests
+import urllib.parse
+from datetime import datetime
 
 TARGET_SERVER='http://localhost:5000'
 REQUEST_URL="/v1/?latitude=%s&longitude=%s&username=%s&sessionid=%s&date=%s"
 
 for i in range(1):
-    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'caspii', 'secret', '2018-03-06%2B23%253A46%253A59'))
-    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'caspii2', 'secret2', '2018-03-06%2B23%253A46%253A59'))
+    date = urllib.parse.quote_plus(datetime.now().strftime('%Y-%m-%d+%H:%M:%S'))
+    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'person1', 'secret', date))
+    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'person2', 'secret2', date))
     # Send wrong secret
-    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'caspii2', 'secret3', '2018-03-06%2B23%253A46%253A59'))
+    requests.get(TARGET_SERVER + REQUEST_URL % (9000, 9000, 'person2', 'secret3', date))
 
 
 
